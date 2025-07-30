364908b02bcb126e93eba4111d34621618fa5dbf
Zeke Osborn <zekeosborn@outlook.com>
2025-07-30 10:41:47 +0000

    feat: add nft contract

---
+++ contracts/src/BattleSynq.sol
@@ -0,0 +1,116 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.27;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721Burnable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import {ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import {ERC721Pausable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
+import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+/// @custom:security-contact zekeosborn200@gmail.com
+contract BattleSynq is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Pausable, Ownable, ERC721Burnable {
+    uint256 private _nextTokenId;
+    string private _defaultTokenURI;
+    uint256 public constant MAX_SUPPLY = 10000;
+
+    event Minted(address indexed to, uint256 indexed tokenId);
+
+    modifier withinSupply() {
+        require(_nextTokenId < MAX_SUPPLY, "Max supply reached");
+        _;
+    }
+
+    constructor(
+        address initialOwner, 
+        string memory initialDefaultTokenURI
+    )
+        ERC721("BattleSynq", "BSQ")
+        Ownable(initialOwner)
+    {
+        _defaultTokenURI = initialDefaultTokenURI;
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function safeMint(address to, string memory uri)
+        public
+        onlyOwner
+        returns (uint256)
+    {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+        emit Minted(to, tokenId);
+        return tokenId;
+    }
+
+    function publicMint()
+        public
+        withinSupply
+        returns (uint256)
+    {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(msg.sender, tokenId);
+        _setTokenURI(tokenId, _defaultTokenURI);
+        emit Minted(msg.sender, tokenId);
+        return tokenId;
+    }
+
+    function defaultTokenURI()
+        public
+        view
+        returns (string memory)
+    {
+        return _defaultTokenURI;
+    }
+
+    function setDefaultTokenURI(string memory newDefaultTokenURI) 
+        public 
+        onlyOwner
+    {
+        _defaultTokenURI = newDefaultTokenURI;
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address to, uint256 tokenId, address auth)
+        internal
+        override(ERC721, ERC721Enumerable, ERC721Pausable)
+        returns (address)
+    {
+        return super._update(to, tokenId, auth);
+    }
+
+    function _increaseBalance(address account, uint128 value)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._increaseBalance(account, value);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
